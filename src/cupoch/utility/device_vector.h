42e69eb0518fdc662e458085a6d9bf9b07f38373
VictorLee <lzs_1993@qq.com>
2021-08-13 20:59:31 +0800

    Add CudaManagedMemoryPool rmmAllocationMode.

--- src/cupoch/utility/device_vector.h
+++ src/cupoch/utility/device_vector.h
@@ -6,10 +6,10 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -59,6 +59,7 @@ enum rmmAllocationMode_t {
     CudaDefaultAllocation = 0,
     PoolAllocation = 1,
     CudaManagedMemory = 2,
+    CudaManagedMemoryPool = 3,
 };
 
 template <typename T>
